3fa42bbe5b90d52029676a2a3f66e43990b98052
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-05-31 10:40:58 +0200

    add basic view for login and register

--- Hè/src/App.vue
+++ Hè/src/App.vue
@@ -6,17 +6,18 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/register">Register</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
