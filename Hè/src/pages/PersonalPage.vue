76207894ebde0f1bec7b029925c1d162940817c7
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-06-03 10:03:30 +0200

    logout implemented

--- Hè/src/pages/PersonalPage.vue
+++ Hè/src/pages/PersonalPage.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import PersonalContainer from "@/components/PersonalContainer.vue"
+import { onMounted, ref } from 'vue'
 import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+const userEmail = ref('')
+const router = useRouter()
+
+const logout = () => {
+  localStorage.removeItem('token')
+  router.push('/login')
+}
+
 
 onMounted(async () => {
-    const token = localStorage.getItem('token');
-    console.log('Token:', token);
-    axios.get('http://localhost:3000/user/verify', {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    }).then((response) => {
-        console.log("valid token"+response.data)
-    })
-    .catch((error) => {
-        console.log("wrong or expired token")
-    });
-});
+  const token = localStorage.getItem('token')
+  console.log('Token:', token)
+  axios.get('http://localhost:3000/user/verify', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    console.log("valid token" + response.data.email)
+    userEmail.value = response.data.email
+  }).catch((error) => {
+    console.log("wrong or expired token")
+    // Redirect to login
+    router.push('login')
+  })
+})
 </script>
 
 <template>
-  <h1>PERSONAL PAGE</h1>
+  <h1>Pagina personale di {{ userEmail }}</h1>
+  <PersonalContainer />
+  <button @click="logout">Logout</button>
 </template>
