1aa83236d7778e5d2950f89f25d42b3e84d92aac
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-06-09 11:21:12 +0200

    fixed logout

--- Hè/src/components/LogoutButton.vue
+++ Hè/src/components/LogoutButton.vue
@@ -1,21 +1,22 @@
 <script lang="ts">
-    import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 
-    export default {
-        setup() {
-            const router = useRouter()
-            
-            const logout = () => {
-                localStorage.removeItem('token')
-                router.push('login')
-            }
-            
-            return {
-                logout
-            }
+export default {
+    setup() {
+        const router = useRouter()
+
+        const logout = () => {
+            localStorage.removeItem('token')
+            router.push('/login')
+        }
+
+        return {
+            logout
         }
     }
+}
 </script>
+
 
 <template>
     <button @click="logout">Logout</button>
