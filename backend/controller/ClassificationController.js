8c077d1c3549a915ed1633d83a40e7b419c7edd0
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-06-10 17:48:41 +0200

    query for classification very tiring

--- backend/controller/ClassificationController.js
+++ backend/controller/ClassificationController.js
@@ -1,9 +1,15 @@
 const Bin = require('../model/Bin');
+const Deposit = require('../model/Deposit');
 const { ObjectId } = require('mongodb');
+const currentDate = new Date();
+const previousMonth = currentDate.getMonth(); // months are zero-based (0 - 11)
+const currentYear = currentDate.getFullYear();
+
 
 const handleClassification = async (req, res) => {
+    const createdAtFiltered = await (await Deposit.find({}, { createdAt: 1 })).filter(d => d.createdAt.getMonth() === (previousMonth - 1) && d.createdAt.getFullYear() === currentYear);
     const idIndifferenziata = new ObjectId('648437bac62e21f98afdf7fe');
-    const indifByMunicip = await Bin.aggregate([
+    const indifByMunicip = await Bin.aggregate([ 
         {
             $lookup: {
               from: "Deposit",
@@ -29,31 +35,25 @@ const handleClassification = async (req, res) => {
             $match: { _id: idIndifferenziata }
         }
       ]);
-
-    /*await Bin.aggregate([
+      const getDeposit = await Deposit.aggregate([
         {
+            $match: {
+              "_id": {
+                  $in: createdAtFiltered.map(d => d._id)
+                }
+            }
+          }
+      ]);
+/*{
             $lookup: {
               from: "Deposit",
               localField: "_id",
               foreignField: "bin",
               as: "deposit"
             }
-        },
-        {
-          $group: {
-            _id: "$typology",
-            bins: { $push: "$$ROOT" }
-          }
-        },
-        { $unwind: "$bins" },
-        {
-            $group: {
-              _id: "$bins.municipality",
-              bins: { $push: "$$ROOT" }
-            }
-          }
-      ]);*/
+        },*/
 
+    //console.log("create",createdAt);
 
     if (!indifByMunicip) return res.status(204).json({ 'message': 'The classification is not available' });
     res.json(indifByMunicip);
